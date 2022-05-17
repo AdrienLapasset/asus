f33f460c59a3adad05e2f8c973b532fbdd72203d
AdrienLapasset <aflapasset@gmail.com>
2022-05-17 10:59:40 +0200

    init

--- index.php
+++ index.php
@@ -4,19 +4,7 @@
   <div class=" w-100">
     <h1>Pages :</h1>
     <div class="list-group">
-      <a class="list-group-item list-group-item-action" href="teasing.php">Teasing</a>
-      <a class="list-group-item list-group-item-action" href="home-1.php">Home - 1</a>
-      <a class="list-group-item list-group-item-action" href="home-2.php">Home - 2</a>
-      <a class="list-group-item list-group-item-action" href="comment-jouer.php">Comment jouer ?</a>
-      <a class="list-group-item list-group-item-action" href="mon-compte.php">Mon compte</a>
-      <a class="list-group-item list-group-item-action" href="creer-compte.php">Creer compte</a>
-      <a class="list-group-item list-group-item-action" href="declarer-un-achat-1.php">Déclarer un achat 1</a>
-      <a class="list-group-item list-group-item-action" href="declarer-un-achat-2.php">Déclarer un achat 2</a>
-      <a class="list-group-item list-group-item-action" href="declarer-un-achat-3.php">Déclarer un achat 3</a>
-      <a class="list-group-item list-group-item-action" href="mes-grilles-actives.php">Grilles actives</a>
-      <a class="list-group-item list-group-item-action" href="mes-grilles-disabled.php">Grilles verrouillées</a>
-      <a class="list-group-item list-group-item-action" href="mentions-legales.php">Mentions légales</a>
-      <a class="list-group-item list-group-item-action" href="reglement.php">Règlement</a>
+      <a class="list-group-item list-group-item-action" href="home.php">Home</a>
     </div>
   </div>
 </div>
