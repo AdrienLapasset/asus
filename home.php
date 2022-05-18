4283b8ff40becfee0c23a51e2e794e5f6ca99a9c
Klemo <clement.lapasset@gmail.com>
2022-05-18 11:42:42 +0200

    homepage text

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
   <div class="container">
     <h2>Partner Incentive</h2>
     <h1>BACK TO WORK</h1>
-    <p>Remportez un maximum de cadeaux pour
+    <p>Remportez un maximum de cadeaux pour<br>
       la rentrée avec ASUS Business</p>
     <a href="">Je participe </a>
   </div>
