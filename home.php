30e3c276e3d0ce8ddfc5c04b32420eecdd86c075
AdrienLapasset <aflapasset@gmail.com>
2022-05-23 13:16:02 +0200

    init mobile page image

--- home.php
+++ home.php
@@ -8,6 +8,7 @@
     <p>Remportez un maximum de cadeaux pour la rentrée avec ASUS Business</p>
     <a class="c-btn c-btn-primary">Je participe <img src="assets/icons/arrow.svg" alt="arrow"></a>
   </div>
+  <img class="d-md-none" src="assets/imgs/bg-home-mobile.jpg" alt="">
 </main>
 
 <?php include 'partials/footer.php'; ?>
