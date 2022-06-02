b9b30b7343bfc5b370738746f1d27033acbce1e6
AdrienLapasset <aflapasset@gmail.com>
2022-06-02 15:03:14 +0200

    global background center

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include("partials/head.php"); ?>
 
 <div class="container d-flex align-items-center justify-content-center py-5">
-  <div class=" w-100">
+  <div class="w-100">
     <h1>Pages :</h1>
     <div class="list-group">
       <a class="list-group-item list-group-item-action" href="home.php">Home</a>
